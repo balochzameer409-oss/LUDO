da0533d19faa9ff309bc55f0ec156c5fd82d1a13
zameer <balochzameer409@gmail.com>
2026-03-04 20:52:04 +0500

    Update ludo.js

--- app/public/js/ludo.js
+++ app/public/js/ludo.js
@@ -1,6 +1,6 @@
 let socket = io(window.location.href.substring(0,window.location.href.length-7));
 
-const room_code = window.location.href.substring(window.location.href.length-8)
+const room_code = window.location.href.substring(window.location.href.length-6)
 const USERNAMES = ['Green Warrior', 'Red Fire', 'Blue Fox', 'Yellow Rhino'];
 const PIECES = [];
 const colors = ["green","red","blue","yellow"];
